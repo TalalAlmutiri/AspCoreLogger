261bf92f3d9d41f8cac48122f3ac2481b1f10da1
TalalAlmutairi <Almutiri.Talal@hotmail.com>
2020-07-04 21:11:01 +0300

    Initial Commit

---
+++ SharedLogger/EventsLog.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLogger
+{
+    // Required
+    //Install-Package Serilog.Sinks.EventLog
+
+    /// <summary>
+    /// Using Serilog in a class library as a shared logger
+    /// </summary>
+    public class EventsLog
+    {
+        private ILogger _logger;
+
+        public EventsLog()
+        {
+            // Configuration of the Serilog logger
+            _logger = new LoggerConfiguration()
+                 .WriteTo.EventLog("Application", manageEventSource: true)
+                  .CreateLogger();
+        }
+        /// <summary>
+        ///  Writing events to Windows events log
+        /// </summary>
+        /// <param name="text"></param>
+        public void WriteLog(string text)
+        {
+            try
+            {
+                _logger.Information(text);
+                _logger.Error(text);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ SharedLogger/LogToFile.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLogger
+{
+    // Required
+    //Install-Package Serilog.Sinks.File
+
+    /// <summary>
+    /// Using Serilog in a class library as a shared logger to write logs into a file
+    /// </summary>
+    public class LogToFile
+    {
+        private ILogger _logger;
+
+        public LogToFile()
+        {
+            _logger = new LoggerConfiguration()
+                     .WriteTo.File("D://log.txt", rollingInterval: RollingInterval.Month)
+                     .CreateLogger();
+        }
+        /// <summary>
+        ///  Writing events to a file
+        /// </summary>
+        /// <param name="text"></param>
+        public void WriteLog(string text)
+        {
+            try
+            {
+
+                _logger.Information(text);
+                _logger.Error(text);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
